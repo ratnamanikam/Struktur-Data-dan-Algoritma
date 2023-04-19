687bc471b1f2720ed2b1b1bf6d4b06585a82be5c
Ratna <127307167+ratnamanikam@users.noreply.github.com>
2023-04-19 12:42:46 +0700

    Create LinkedList.h

---
+++ Linked List/LinkedList.h
@@ -0,0 +1,34 @@
+class Node{
+	
+	public :
+		int value; 
+		Node *next;	
+		
+		Node(){ 
+			next = NULL;	
+		}
+};
+
+class LinkedList {
+	
+	public:
+		
+		Node *head;
+		Node *tail;
+		
+		void insertToHead(int);
+		void insertToTail(int);
+		void insertAfter(int, int); 
+	
+		
+		void deleteFromHead();
+		void deleteFromTail();
+		void deleteByValue(int);
+		bool isEmpty();
+		
+		void printAll();
+		
+		LinkedList(){
+			head = tail = NULL;
+		}
+};
